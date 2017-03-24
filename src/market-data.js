b9a0a7026a02f00f3881d268983ffde09a024ed3
tomgp <tom.g.pearson@gmail.com>
2017-03-24 10:26:27 +0000

    allow the getting of performance metrics

--- src/market-data.js
+++ src/market-data.js
@@ -1,26 +1,33 @@
 import * as d3Request from 'd3-request';
 
 export default function(key) {
-  if (!key) console.error('no key specified');
+  if (!key) console.error('no source ID specified');
 
   const data = {};
 
   var callback = function(d){ console.log('DATA', d) };
   var error = function(e){ console.log('ERROR', e) };
 
-  function getter(symbol){
-    if( data[symbol] !== undefined){
-      callback( data[symbol] );
+  function getter(symbol, metrics){
+    if(metrics === undefined) metrics = false;
+    const hashkey = symbol + String(metrics);
+
+    if( data[hashkey] !== undefined){
+      callback( data[key] );
     }else{
-      d3Request.json('http://markets.ft.com/research/webservices/securities/v1/quotes?symbols=' + symbol + '&source=' + key, 
-      function(err, jsonData){
-        if (err !== null){
-          error(err);
-          return;
-        }
-        data[symbol] = jsonData;
-        callback(data[symbol]);
-      }) 
+      var URL = 'http://markets.ft.com/research/webservices/securities/v1/quotes?symbols=' + symbol + '&source=' + key;
+      if(metrics){
+        URL = 'http://markets.ft.com/research/webservices/securities/v1/performance-metrics?symbols=' + symbol + '&source=' + key;
+      }
+
+      d3Request.json(URL, function(err, jsonData){
+          if (err !== null){
+            error(err);
+            return;
+          }
+          data[hashkey] = jsonData;
+          callback(data[hashkey]);
+        })
     }
   }
 
--- test/market-test.js
+++ test/market-test.js
@@ -2,6 +2,7 @@ var tape = require("tape"),
     marketdata = require("../");
 
 tape("marketdata() returns the answer to the ultimate question of life, the universe, and everything.", function(test) {
+  console.log(process.env.MARKETKEY);
   var getter = marketdata.marketdata(process.env.MARKETKEY)
     .callback(function(d){
       console.log(d.data.items[0].basic.name + ' stock price');
@@ -9,7 +10,7 @@ tape("marketdata() returns the answer to the ultimate question of life, the univ
     });
 
   getter('AAPL');
-  
+
   //test.equal(marketdata.marketdata(), 42);
   test.end();
 });
